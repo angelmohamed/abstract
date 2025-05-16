adf2a15d5b34bb7bd704bc0582d7b0ef8e88c7a4
mariselvam <mariselvam@wealwin.com>
2025-05-16 13:29:36 +0530

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
 # #!/bin/bash
-# set -e
+set -e
 
-# TMP_DIR="./next-deploy"
-# REPO="git@gitlab.com:wealwinprojects/sonotrade-frontend-2025.git"
-# SSH_KEY="/Users/mariselvam/Documents/project/sonotrade/ref/developerssh.pem"
-# BRANCH="main"
+TMP_DIR="./next-deploy"
+REPO="git@gitlab.com:wealwinprojects/sonotrade-frontend-2025.git"
+SSH_KEY="/Users/mariselvam/Documents/project/sonotrade/ref/developerssh.pem"
+BRANCH="main"
 
-# yarn install
-# yarn build
+yarn install
+yarn build
 
-# # Clone repo into new temp folder
-# echo "📁 Cloning repo into temp dir: $TMP_DIR"
-# GIT_SSH_COMMAND="ssh -i $SSH_KEY" git clone --branch "$BRANCH" "$REPO" "$TMP_DIR" || exit 1
+# Clone repo into new temp folder
+echo "📁 Cloning repo into temp dir: $TMP_DIR"
+GIT_SSH_COMMAND="ssh -i $SSH_KEY" git clone --branch "$BRANCH" "$REPO" "$TMP_DIR" || exit 1
 
-# # Copy build into repo
-# echo "📦 Copying build output to repo..."
-# rm -rf "$TMP_DIR/build"
-# cp -r .vercel "$TMP_DIR/"
+# Copy build into repo
+echo "📦 Copying build output to repo..."
+rm -rf "$TMP_DIR/build"
+cp -r .vercel "$TMP_DIR/"
 
 # Commit & push
 git -C "$TMP_DIR" add .
 git -C "$TMP_DIR" commit -m "🚀 Deploy build $(date +%F_%T)" || echo "⚠️ Nothing to commit"
 GIT_SSH_COMMAND="ssh -i $SSH_KEY" git -C "$TMP_DIR" push
 
-# # Clean up
-# # rm -rf "$TMP_DIR"
+# Clean up
+rm -rf "$TMP_DIR"
 
-# echo "✅ Deployment complete."
+echo "✅ Deployment complete."
