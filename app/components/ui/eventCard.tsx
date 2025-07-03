d950dc9ff12fc4e4e89cccad475c17c7fec26ca2
vengatesh <vengatesh@wealwin.com>
2025-07-03 19:33:59 +0530

    UI

--- app/components/ui/eventCard.tsx
+++ app/components/ui/eventCard.tsx
@@ -81,8 +81,7 @@ const EventCard: React.FC<EventCardProps> = ({
 
   return (
     <Card
-      className="flex flex-col justify-between w-full h-[200px] sm:h-[200px]"
-      style={{ backgroundColor: "#161616" }}
+      className="flex flex-col justify-between w-full h-[200px] sm:h-[200px] bg-transparent"      
     >
       <CardHeader className="sm:pt-3 sm:pl-3 pl-3 sm:pr-3 pr-3 pt-3 pb-0">
         <CardTitle style={{ lineHeight: "1.5" }}>
@@ -116,82 +115,87 @@ const EventCard: React.FC<EventCardProps> = ({
               {question}
             </div>
             {/* <div>{probability && decimalToPercentage(probability)}%</div> */}
-            {status == "active" && (<div>{probability ? `${probability}%` : ""}</div>)}
+            {status == "active" && (
+              <div>{probability ? `${probability}%` : ""}</div>
+            )}
           </div>
         </CardTitle>
       </CardHeader>
 
-      { status == "active" ? (
-        <CardContent className="pb-0 sm:pl-3 pl-3 sm:pr-3 pr-3">
-          <div className="pb-4 pt-1">
-            {(probability && probability != 0) ? (
-              <Progress
-                // value={probability && decimalToPercentage(probability)}
-                value={probability}
-                className="w-[100%]"
-              />
-            ) : (
-              <Progress
-                // value={probability && decimalToPercentage(probability)}
-                value={0}
-                className="w-[100%]"
-              />
-            )} 
-          </div>
-          <div
-            className="pb-0"
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
-            {/* Yes Button */}
+      <CardContent className="pb-0 sm:pl-3 pl-3 sm:pr-3 pr-3">
+        {status == "active" ? (
+          <>
+            <div className="pb-4 pt-1">
+              {probability && probability != 0 ? (
+                <Progress
+                  // value={probability && decimalToPercentage(probability)}
+                  value={probability}
+                  className="w-[100%]"
+                />
+              ) : (
+                <Progress
+                  // value={probability && decimalToPercentage(probability)}
+                  value={0}
+                  className="w-[100%]"
+                />
+              )}
+            </div>
             <div
-              className="text-[12px]"
+              className="pb-0"
               style={{
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                width: "48%",
+                justifyContent: "space-between",
+                width: "100%",
               }}
             >
-              <Button
-                onClick={handleYesClick}
-                className="w-full mb-1 bg-[#152632] text-[#7dfdfe] hover:bg-[#7dfdfe] hover:text-[#152632] transition-colors duration-300 rounded-full capitalize"
+              {/* Yes Button */}
+              <div
+                className="text-[12px]"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  width: "48%",
+                }}
               >
-                {yesButtonLabel}
-              </Button>
-            </div>
+                <Button
+                  onClick={handleYesClick}
+                  className="w-full mb-1 bg-[#152632] text-[#7dfdfe] hover:bg-[#7dfdfe] hover:text-[#152632] transition-colors duration-300 rounded-full capitalize"
+                >
+                  {yesButtonLabel}
+                </Button>
+              </div>
 
-            {/* No Button */}
-            <div
-              className="text-[12px]"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                width: "48%",
-              }}
-            >
-              <Button
-                onClick={handleNoClick}
-                className="w-full mb-1 bg-[#321b29] text-[#ec4899] hover:bg-[#ec4899] hover:text-[#321b29] transition-colors duration-300 rounded-full capitalize"
+              {/* No Button */}
+              <div
+                className="text-[12px]"
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  width: "48%",
+                }}
               >
-                {noButtonLabel}
-              </Button>
+                <Button
+                  onClick={handleNoClick}
+                  className="w-full mb-1 bg-[#321b29] text-[#ec4899] hover:bg-[#ec4899] hover:text-[#321b29] transition-colors duration-300 rounded-full capitalize"
+                >
+                  {noButtonLabel}
+                </Button>
+              </div>
             </div>
-          </div>
-        </CardContent>
+          </>
         ) : status == "resolved" ? (
           <>
-            <span>Outcome</span>
-            <span>No</span>
+            <div className="pb-4 pt-1">
+              <p>Outcome:</p>
+              <p className="text-[#ec4899]">No</p>
+            </div>
           </>
-        ) : status == "closed" && (
-          <span>Closed</span>
-        ) 
-      }
+        ) : (
+          status == "closed" && <span>Closed</span>
+        )}
+      </CardContent>
 
       <CardFooter className="sm:pl-3 pl-3 sm:pr-3 pr-3 pb-2 overflow-hidden">
         <div
@@ -212,7 +216,9 @@ const EventCard: React.FC<EventCardProps> = ({
               textOverflow: "ellipsis",
             }}
           >
-            {(status == "active" && totalPool) && <CardDescription>{totalPool} Vol</CardDescription>}
+            {status == "active" && totalPool && (
+              <CardDescription>{totalPool} Vol</CardDescription>
+            )}
           </span>
           {/* <Button
             className="p-1 h-6 w-6  z-10 rounded"
