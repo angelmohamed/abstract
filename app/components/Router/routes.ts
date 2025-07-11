e32b4141199d7e5fbb89ac728c5ea2504ee6e8cb
mariselvam <prasanna@wealwin.com>
2025-07-11 16:56:29 +0530

    conditional route middleware implementation

---
+++ app/components/Router/routes.ts
@@ -0,0 +1,14 @@
+export const authRoutes = [
+    "/login",
+    "/register",
+    "/forgot-password",
+    "/reset-password/[token]",
+]
+ 
+export const protectedRoutes: string[] = [
+    "/settings",
+    "/portfolio",
+    "/profile",
+    // "/cp/account/[id]/perp",
+    // "/cp/account/[id]/spot",
+]
